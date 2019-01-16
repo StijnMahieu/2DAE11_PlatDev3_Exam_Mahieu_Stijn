816106be29f691d1e5b99320d17e91efb5c6a85f
StijnMahieu <stijn.mahieu2@student.howest.be>
2019-01-16 16:24:29 +0100

    AI + navmesh setup

--- Assets/Scripts/AIBehaviour.cs
+++ Assets/Scripts/AIBehaviour.cs
@@ -7,30 +7,61 @@ public class AIBehaviour : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        //        _rootNode =
-        //new SelectorNode
-        //(
-        //            new SequenceNode
-        //            (
-        //                new ConditionNode(IsDead)
-
-        //                new ActionNode(Test)
-        //            )
-        //        )
-
-
-        //        StartCoroutine(RunTree());
-        //    }
-
-
-        //    bool IsDead()
-        //    {
-        //        return true;
-        //    }
-        //    IEnumerator Test()
-        //    {
-
-        //        yield return NodeResult.Succes;
-        //    }
+        _rootNode =
+    new SelectorNode
+    (
+            new SequenceNode
+            (
+                new ConditionNode(IsDead),
+                new ActionNode(PlayDeathAnimation)
+            ),
+            new SequenceNode
+            (
+                new ConditionNode(IsHit),
+                new ActionNode(PlayHitAnimation)
+            ),
+            new SequenceNode
+            (
+                new ConditionNode(SeesPlayer),
+                new ActionNode(ShootPlayer)
+            ),
+            new ActionNode(Roaming)
+    );
+        StartCoroutine(RunTree());
+    }
+    IEnumerator RunTree()
+    {
+        while(Application.isPlaying)
+        {
+            yield return _rootNode.Tick();
+        }
+    }
+    bool IsDead()
+    {
+        return true;
+    }
+    bool IsHit()
+    {
+        return true;
+    }
+    bool SeesPlayer()
+    {
+        return true;
+    }
+    IEnumerator <NodeResult> PlayDeathAnimation()
+    {
+        yield return NodeResult.Succes;
+    }
+    IEnumerator<NodeResult> PlayHitAnimation()
+    {
+        yield return NodeResult.Succes;
+    }
+    IEnumerator<NodeResult> ShootPlayer()
+    {
+        yield return NodeResult.Succes;
+    }
+    IEnumerator<NodeResult> Roaming()
+    {
+        yield return NodeResult.Succes;
     }
 }
